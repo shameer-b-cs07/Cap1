a1a22069881cad8b2892258b3ec349b86a7e7273
shameer-b-cs07 <111631197+shameer-b-cs07@users.noreply.github.com>
2023-03-23 15:55:45 +0530

    Add files via upload

---
+++ email-notification.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+    #new_pods=$(kubectl get pods --all-namespaces --field-selector=status.phase=Running -o json | jq '.items[].metadata.name' | grep -v "email-notification" | grep -v "kube-proxy" | sort)
+    new_pods=$(kubectl get pods --all-namespaces --field-selector=status.phase=Running -o json | jq '.items[].metadata.name' | grep -v "email-notification" | grep -v "kube-proxy" |  grep -v "coredns" |grep -v "etcd" | grep -v "kube-apiserver" | grep -v "kube-controller" | grep -v "kube-scheduler" | grep -v "weave" |sort)
+    for pod in $new_pods
+    do
+        if ! grep -q "$pod" /tmp/k8s-pods
+        then
+            echo "New Static pod created: $pod" | mailx -s "New Static Pod Created" shameerbabukm@gmail.com
+            echo "$pod" >> /tmp/k8s-pods
+        fi
+    done
+    sleep 10
+    rm -rf /tmp/k8s-pods
+done
